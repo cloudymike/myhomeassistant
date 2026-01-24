32a68428d9c3cfdbaa3260a176c23f6d240f8906
cloudymike <mikael@cloudyforecast.com>
2026-01-24 09:02:54 -0800

    Working

---
+++ esp32c3/switch12v/README.md
@@ -0,0 +1,9 @@
+# Example swith using the blue LED on ESP board
+
+This is a very simple example to use as a base for controlling 
+something from homeassistant. It will allow you 
+to control the blue LED on the board. The code includes
+anything needed for self discovery
+
+The setup assume that you are running the home assistant server on the 
+local server.
---
+++ esp32c3/switch12v/main.py
@@ -0,0 +1,61 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025 lbuque
+#
+# SPDX-License-Identifier: MIT
+
+
+import time
+import mqtthost
+import wlan
+
+import mha
+
+import machine
+import binascii
+
+DaSwitch = machine.Pin(3, machine.Pin.OUT)
+
+
+#aDeviceString=binascii.hexlify(machine.unique_id()).decode('utf-8')
+aDeviceString="001122AABBCD"
+device = mha.HADevice(aDeviceString)  
+#Create a random name at start
+#device = mha.HADevice()  
+
+
+sta=wlan.do_connect('gardenswith')
+
+mqtt = mha.HAMqtt(device)
+
+device.set_name("gardenswitch")
+device.set_software_version("0.1.0")
+device.set_manufacturer("Espressif")
+device.set_model("ESP32-C3")
+
+switch = mha.HASwitch("garden_switch")
+
+switch.set_current_state(True)
+switch.set_name("switch12V")
+switch.set_icon("mdi:lightbulb")
+
+def on_switch_command(sender: mha.HASwitch, state: bool):
+    print("Switch state:", state)
+    sender.set_state(state)
+    # ON is OFF for ESP32C3 LED. Go figure
+    if state:
+        DaSwitch.on()
+    else:
+        DaSwitch.off()
+        
+    # to some action here
+
+switch.on_command(on_switch_command)
+
+mqtt.begin(mqtthost.MQTT_HOST)
+
+last_time = time.time()
+
+while True:
+    mqtt.loop()
+#    if time.time() - last_time > 5:
+#        switch.set_state(not switch.get_current_state())
+#        last_time = time.time()
